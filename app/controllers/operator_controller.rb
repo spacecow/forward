5f1ce6c26844566cdc4a91b40ebef53c21ff52e9
Johan Sveholm <jsveholm@gmail.com>
2011-05-13 16:13:41 +0900

    five fields for email

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+#require 'rpam'
 
 class OperatorController < ApplicationController
+  #include Rpam
+
   def login
   end
 
@@ -12,6 +15,7 @@ class OperatorController < ApplicationController
   def connect
     if authpam(params[:username],params[:password])
       session[:username] = params[:username]
+      session[:password] = params[:password]
       redirect_to edit_path, :notice => notify(:logged_in)
     else
       redirect_to login_path, :alert => alert2(:incorrect,
@@ -21,17 +25,35 @@ class OperatorController < ApplicationController
 
   def edit
     if session[:username]
-      p File.open("test.txt").readlines
+      IO.popen("/usr/local/sbin/chfwd -g #{session[:username]}", 'r+') do |pipe|
+        pipe.write(session[:password])
+        pipe.close_write
+        @input = convert_in(pipe.read) 
+      end
     else
       redirect_to login_path, :alert => alert(:unauthorized_access)
     end
   end
 
   def update
+    IO.popen("/usr/local/sbin/chfwd -s #{session[:username]}", 'r+') do |pipe|
+      pipe.write(session[:password])
+      puts params
+    end
     redirect_to edit_path
   end
 
   private
     def authpam(user,pass); pass == "correct" ? true : false end
+    def convert_in(s)
+      ret = []
+      s.split("\n").each do |add|
+        ret << {:address => add.chomp}
+      end
+      (5-ret.size).times do |add|
+        ret << {:address => nil}
+      end
+      ret
+    end
 end
 
